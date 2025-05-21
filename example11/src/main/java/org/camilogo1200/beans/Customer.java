e3e8b1b040b00098e860ad0433b595f3078d2012
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2025-05-20 23:00:54 -0500

    Added example 11 - Injecting @Components using a variant for  @Autowired using a setter method

---
+++ example11/src/main/java/org/camilogo1200/beans/Customer.java
@@ -0,0 +1,44 @@
+package org.camilogo1200.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class Customer {
+  private int id;
+  private String name;
+  private String lastName;
+  private String address;
+  private String phone;
+  private String email;
+
+  @Autowired
+  private Property property;
+  private Vehicle vehicle;
+
+  @Autowired
+  public void setVehicle(Vehicle vehicle) {
+    this.vehicle = vehicle;
+  }
+
+  @Override
+  public String toString() {
+    return "Customer{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", address='" + address + '\'' +
+        ", phone='" + phone + '\'' +
+        ", email='" + email + '\'' + '\n' +
+        ", property=" + property + '\n' +
+        ", vehicle=" + vehicle + '\n' +
+        '}';
+  }
+}
---
+++ example11/src/main/java/org/camilogo1200/beans/Wallet.java
@@ -0,0 +1,12 @@
+package org.camilogo1200.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wallet {
+  public int id;
+}
